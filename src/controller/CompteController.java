a898be3ee71e4f8d8cdd99f904417c6e5f027340
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-09-20 15:19:18 +0100

    add gestion des exception

--- src/controller/CompteController.java
+++ src/controller/CompteController.java
@@ -19,112 +19,137 @@ public class CompteController {
     }
 
     public void creerCompteCourant(double solde,double decouvert){
-        String numeroCompte = genereteCompteur();
-        CompteCourant cc = new CompteCourant(numeroCompte,solde,new Date(),new ArrayList<>(),decouvert);
-        comptes.put(cc.getNumeroCompte(),cc);
-        System.out.println(" Compte courant créé avec succès !");
+        try {
+            if (solde < 0) throw new IllegalArgumentException("Le solde initial ne peut pas être négatif !");
+            if (decouvert < 0) throw new IllegalArgumentException("Le découvert ne peut pas être négatif !");
+            String numeroCompte = genereteCompteur();
+            CompteCourant cc = new CompteCourant(numeroCompte, solde, new Date(), new ArrayList<>(), decouvert);
+            comptes.put(cc.getNumeroCompte(), cc);
+            System.out.println(" Compte courant créé avec succès !");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }
     }
     public void creerCompteEpargne(double solde ){
-        String numeroCompte = genereteCompteur();
-        CompteEpargne ce=new CompteEpargne(numeroCompte,solde,new Date(),new ArrayList<>());
-        comptes.put(ce.getNumeroCompte(),ce);
-        System.out.println(" Compte Epargne créé avec succès !");
+        try{
+            if (solde < 0) throw new IllegalArgumentException("Le solde initial ne peut pas être négatif !");
+            String numeroCompte = genereteCompteur();
+            CompteEpargne ce=new CompteEpargne(numeroCompte,solde,new Date(),new ArrayList<>());
+            comptes.put(ce.getNumeroCompte(),ce);
+            System.out.println(" Compte Epargne créé avec succès !");
+        }catch (IllegalArgumentException e){
+            System.out.println("Erreur : " + e.getMessage());
+        }
     }
 
 
     public  void verse(String numeroCompte,double montant,Source source){
-        Compte compte=comptes.get(numeroCompte);
-        if (compte == null){
-            System.out.println("Compte introuvable : " + numeroCompte);
-            return;
+        try{
+            Compte compte=comptes.get(numeroCompte);
+            if (compte == null) throw new IllegalArgumentException("Compte introuvable : " + numeroCompte);
+
+            if (montant <= 0)throw new IllegalArgumentException("Le montant doit être positif.");
+            compte.verser(montant,source);
+        }catch (IllegalArgumentException e){
+            System.out.println("Erreur versement : " + e.getMessage());
         }
-        if (montant <= 0) {
-            System.out.println("Le montant doit être positif.");
-            return;
-        }
-        compte.verser(montant,source);
 //        System.out.println("Versement de " + montant + " DH effectué sur le compte " + numeroCompte);
     }
     public void retirer(String numeroCompte,double montant,Destination destination){
-        Compte compte=comptes.get(numeroCompte);
-        if (numeroCompte == null){
-            System.out.println("Compte introuvable : " + numeroCompte);
-            return;
+        try {
+            Compte compte=comptes.get(numeroCompte);
+            if (numeroCompte == null) throw new IllegalArgumentException("Compte introuvable : " + numeroCompte);
+            if (montant <= 0) throw new IllegalArgumentException("Le montant doit être positif. " );
+            compte.retirer(montant,destination);
+        }catch (IllegalArgumentException e){
+            System.out.println("Erreur Retirer : " + e.getMessage());
         }
-        if (montant <= 0) {
-            System.out.println("Le montant doit être positif.");
-            return;
-        }
-        compte.retirer(montant,destination);
+
     }
     public void virement(String numeroSource, String numeroDestination, double montant){
-        Compte source=comptes.get(numeroSource);
-        Compte destination = comptes.get(numeroDestination);
-        if (source == null) {
-            System.out.println("Compte source introuvable : " + numeroSource);
-            return;
-        }
-        if (destination == null) {
-            System.out.println("Compte destination introuvable : " + numeroDestination);
-            return;
-        }
-        if (montant <= 0) {
-            System.out.println("Le montant doit être positif.");
-            return;
-        }
-        if(source.getSolde() < montant){
-            System.out.println("Solde insuffisant dans le compte source.");
-            return;
-        }
-        source.retirer(montant,VIREMENTSORTANT);
-        destination.verser(montant, VIREMENTEXTERNE);
+        try {
+            Compte source = comptes.get(numeroSource);
+            Compte destination = comptes.get(numeroDestination);
+            if (source == null) throw new IllegalArgumentException("Compte source introuvable : " + numeroSource);
+            if (destination == null) throw new IllegalArgumentException("Compte destination introuvable : " + numeroDestination);
+            if (montant <= 0) throw new IllegalArgumentException("Le montant doit être positif.");
+            if (source.getSolde() < montant) throw new IllegalArgumentException("Solde insuffisant dans le compte source.");
 
-        source.getListeOperations().add(
-                new Retrait(VIREMENTSORTANT, UUID.randomUUID(), new Date(), montant)
-        );
-        destination.getListeOperations().add(
-                new Versement(UUID.randomUUID(), new Date(), montant, VIREMENTEXTERNE)
-        );
-        System.out.println("Virement de " + montant + " DH effectué avec succès !");
+            source.retirer(montant, VIREMENTSORTANT);
+            destination.verser(montant, VIREMENTEXTERNE);
 
-    };
+            source.getListeOperations().add(new Retrait(VIREMENTSORTANT, UUID.randomUUID(), new Date(), montant));
+            destination.getListeOperations().add(new Versement(UUID.randomUUID(), new Date(), montant, VIREMENTEXTERNE));
+
+            System.out.println("Virement de " + montant + " DH effectué avec succès !");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erreur virement : " + e.getMessage());
+        }
+    }
     public void consulterSolde(String numeroCompte){
-        Compte compte=comptes.get(numeroCompte);
-        if (compte== null){
-            System.out.println("Compte source introuvable : " + numeroCompte);
-            return;
+        try {
+            Compte compte = comptes.get(numeroCompte);
+            if (compte == null) throw new IllegalArgumentException("Compte introuvable : " + numeroCompte);
+            System.out.println("Compte : " + compte.getNumeroCompte());
+            System.out.println("Type : " + compte.getClass().getSimpleName());
+            System.out.println("Solde : " + compte.getSolde() + " DH");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erreur : " + e.getMessage());
         }
-        System.out.println("------------------------------------------------");
-        System.out.println("Compte : " + compte.getNumeroCompte());
-        System.out.println("Type : " + compte.getClass().getSimpleName());
-        System.out.println("Solde actuel : " + compte.getSolde() + " DH");
-        System.out.println("------------------------------------------------");
-    };
+    }
     public void afficherDetails(String numeroCompte){
-        Compte compte=comptes.get(numeroCompte);
-        if (compte== null){
-            System.out.println("Compte source introuvable : " + numeroCompte);
-            return;
+        try {
+            Compte compte = comptes.get(numeroCompte);
+            if (compte == null) throw new IllegalArgumentException("Compte source introuvable : " + numeroCompte);
+            compte.afficherDetails();
+        }catch ( IllegalArgumentException e){
+            System.out.println("Erreur : " + e.getMessage());
         }
-        compte.afficherDetails();
     };
-    public void afficherOperations(String numeroCompte){
-        Compte compte=comptes.get(numeroCompte);
-        if (compte== null){
-            System.out.println("Compte source introuvable : " + numeroCompte);
-            return;
+    public void afficherOperations(String numeroCompte) {
+        try {
+            Compte compte = comptes.get(numeroCompte);
+            if (compte == null) {
+                throw new IllegalArgumentException("Compte introuvable : " + numeroCompte);
+            }
+
+            System.out.println("------------------------------------------------");
+            System.out.println("Historique des opérations :");
+            if (compte.getListeOperations().isEmpty()) {
+                System.out.println("Aucune opération effectuée.");
+            } else {
+                for (Operation op : compte.getListeOperations()) {
+                    System.out.println(op);
+                }
+            }
+            System.out.println("------------------------------------------------");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erreur : " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Une erreur inattendue est survenue lors de l'affichage des opérations.");
         }
-        System.out.println("------------------------------------------------");
-        System.out.println("Historique des opérations :");
-        if (compte.getListeOperations().isEmpty()) {
-            System.out.println("Aucune opération effectuée.");
-        } else {
-            compte.getListeOperations().forEach(System.out::println);
-        }
-        System.out.println("------------------------------------------------");
-    };
+    }
+
     public boolean existeCompte(String numeroCompte) {
         return comptes.containsKey(numeroCompte);
+    }
+    public void afficherAllComptes() {
+        try {
+            if (comptes == null || comptes.isEmpty()) {
+                System.out.println("Aucun compte disponible.");
+                return;
+            }
+            System.out.println("Liste de tous les comptes :");
+            int i = 1;
+            for (Compte cm : comptes.values()) {
+                System.out.println(i + " - " + cm);
+                i++;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Une erreur est survenue lors de l'affichage des comptes : " + e.getMessage());
+        }
     }
 
 }
--- src/controller/ConsoleUI.java
+++ src/controller/ConsoleUI.java
@@ -1,6 +1,6 @@
 package controller;
 
-// Application.java
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import controller.CompteController;
 import util.Source;
@@ -11,26 +11,34 @@ public class ConsoleUI {
     private final CompteController compteController = new CompteController();
 
     public void start() {
-
-            int choix;
-            do {
+        int choix = -1;
+        do {
+            try {
                 afficherMenuPrincipal();
                 choix = scnr.nextInt();
                 scnr.nextLine();
 
                 switch (choix) {
-                    case 1 : creerCompte(); break;
-                    case 2 : managerCompte();break;
-                    case 0 : System.out.println("Au revoir !");break;
-                    default : System.out.println("Option invalide !");break;
+                    case 1: creerCompte(); break;
+                    case 2: managerCompte(); break;
+                    case 3: compteController.afficherAllComptes();break;
+                    case 0: System.out.println("Au revoir !"); break;
+                    default: System.out.println("Option invalide !"); break;
                 }
-            } while (choix != 0);
+            } catch (InputMismatchException e) {
+                System.out.println("Veuillez entrer un nombre valide !");
+                scnr.nextLine(); // vider le buffer
+            } catch (Exception e) {
+                System.out.println("Une erreur est survenue : " + e.getMessage());
+            }
+        } while (choix != 0);
     }
 
     private void afficherMenuPrincipal() {
         System.out.println("------------------------------------------------");
         System.out.println("1 - Créer un compte");
         System.out.println("2 - Manager Votre compte");
+        System.out.println("3 - Afficher List comptes");
         System.out.println("0 - Quitter");
         System.out.println("------------------------------------------------");
         System.out.print("Choisissez une option : ");
@@ -43,13 +51,15 @@ public class ConsoleUI {
             System.out.print("Votre choix : ");
             int typeCompte = scnr.nextInt();
             scnr.nextLine();
+
             if (typeCompte != 1 && typeCompte != 2) {
-                System.out.println(" Choix incorrect, vous devez entrer 1 ou 2.");
-                return;
+                throw new IllegalArgumentException("Choix incorrect, vous devez entrer 1 ou 2.");
             }
+
             System.out.print("Solde initial : ");
             double solde = scnr.nextDouble();
-            switch (typeCompte){
+
+            switch (typeCompte) {
                 case 1:
                     System.out.print("Découvert autorisé : ");
                     double decouvert = scnr.nextDouble();
@@ -58,10 +68,10 @@ public class ConsoleUI {
                 case 2:
                     compteController.creerCompteEpargne(solde);
                     break;
-                default:
-                    System.out.print("Choix incorrect , Vous devez entrer 1 ou 2.");
-                    break;
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Vous devez entrer un nombre !");
+            scnr.nextLine();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -69,71 +79,93 @@ public class ConsoleUI {
 
 
     private void managerCompte() {
-        System.out.print("Numéro du compte (CPT-XXXXX): ");
-        String numCompte = scnr.nextLine();
+        try {
+            System.out.print("Numéro du compte (CPT-XXXXX): ");
+            String numCompte = scnr.nextLine();
 
-        if (!compteController.existeCompte(numCompte)) {
-            System.out.println("Ce compte n'existe pas. Veuillez créer un compte d'abord !");
-            return;
-        }
+            if (!compteController.existeCompte(numCompte)) {
+                throw new IllegalArgumentException("Ce compte n'existe pas. Veuillez créer un compte d'abord !");
+            }
 
-        System.out.println("1 - Verser");
-        System.out.println("2 - Retirer");
-        System.out.println("3 - Virement");
-        System.out.println("4 - Consulter solde");
-        System.out.println("5 - Afficher Operations");
-        System.out.println("6 - Afficher détails du compte");
-        System.out.print("Votre choix : ");
+            System.out.println("1 - Verser");
+            System.out.println("2 - Retirer");
+            System.out.println("3 - Virement");
+            System.out.println("4 - Consulter solde");
+            System.out.println("5 - Afficher Operations");
+            System.out.println("6 - Afficher détails du compte");
+            System.out.print("Votre choix : ");
 
-        int typeOperation = scnr.nextInt();
-        scnr.nextLine();
+            int typeOperation = scnr.nextInt();
+            scnr.nextLine();
 
-        switch (typeOperation) {
-            case 1 :
-                System.out.print("Montant à verser : ");
-                double montantVerse = scnr.nextDouble();
-                scnr.nextLine();
-                for (Source s : Source.values()) {
-                    System.out.println("- " + s);
-                }
-                System.out.print("Votre choix : ");
-                String choixSource = scnr.nextLine().toUpperCase();
-                try {
-                    Source source = Source.valueOf(choixSource);
-                    compteController.verse(numCompte, montantVerse, source);
-                } catch (IllegalArgumentException e) {
-                    System.out.println("Type de versement invalide !");
-                }
-                break;
+            switch (typeOperation) {
+                case 1:
+                    try {
+                        System.out.print("Montant à verser : ");
+                        double montantVerse = scnr.nextDouble();
+                        scnr.nextLine();
 
-            case 2 :
-                System.out.print("Montant a retirer : ");
-                double montantRetriet = scnr.nextDouble();
-                scnr.nextLine();
-                for (Destination ds : Destination.values()) {
-                    System.out.println("- " + ds);
-                }
-                System.out.print("Votre choix : ");
-                String choixDes = scnr.nextLine().toUpperCase();
-                try {
-                    Destination destination = Destination.valueOf(choixDes);
-                    compteController.retirer(numCompte, montantRetriet, destination);
-                } catch (IllegalArgumentException e) {
-                    System.out.println("Type de Retirer invalide !");
-                }
-                break;
+                        for (Source s : Source.values()) {
+                            System.out.println("- " + s);
+                        }
+                        System.out.print("Votre choix : ");
+                        String choixSource = scnr.nextLine().toUpperCase();
+                        Source source = Source.valueOf(choixSource);
 
-            case 3 :
-                System.out.print("Numero Compte Destinataire :");
-                String numCompteDes = scnr.nextLine();
-                System.out.print("montant : ");
-                double montantVerseOuDes = scnr.nextDouble();
-                compteController.virement(numCompte, numCompteDes, montantVerseOuDes);
-                break;
-            case 4 : compteController.consulterSolde(numCompte);break;
-            case 5 : compteController.afficherOperations(numCompte);break;
-            case 6 : compteController.afficherDetails(numCompte);break;
-            default : System.out.println("Option invalide !");break;
+                        compteController.verse(numCompte, montantVerse, source);
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Type de versement invalide !");
+                    }
+                    break;
+
+                case 2:
+                    try {
+                        System.out.print("Montant a retirer : ");
+                        double montantRetrait = scnr.nextDouble();
+                        scnr.nextLine();
+
+                        for (Destination ds : Destination.values()) {
+                            System.out.println("- " + ds);
+                        }
+                        System.out.print("Votre choix : ");
+                        String choixDes = scnr.nextLine().toUpperCase();
+                        Destination destination = Destination.valueOf(choixDes);
+
+                        compteController.retirer(numCompte, montantRetrait, destination);
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Type de retrait invalide !");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Numero Compte Destinataire : ");
+                    String numCompteDes = scnr.nextLine();
+                    System.out.print("Montant : ");
+                    double montantVirement = scnr.nextDouble();
+                    compteController.virement(numCompte, numCompteDes, montantVirement);
+                    break;
+
+                case 4:
+                    compteController.consulterSolde(numCompte);
+                    break;
+
+                case 5:
+                    compteController.afficherOperations(numCompte);
+                    break;
+
+                case 6:
+                    compteController.afficherDetails(numCompte);
+                    break;
+
+                default:
+                    System.out.println("Option invalide !");
+                    break;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Vous devez entrer un nombre !");
+            scnr.nextLine();
+        } catch (IllegalArgumentException e) {
+            System.out.println( e.getMessage());
         }
     }
 }
